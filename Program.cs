857c62e7a0308ce4ebac0a7f67e9d097dfbad81d
ividyon <1077140+ividyon@users.noreply.github.com>
2026-01-05 15:53:30 +0100

    feat: Timestamp endpoint

--- Program.cs
+++ Program.cs
@@ -14,28 +14,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/timestamp", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return new Timestamp(DateTime.UtcNow);
 })
-.WithName("GetWeatherForecast");
+.WithName("GetTimestamp");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record Timestamp(DateTime Date)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
